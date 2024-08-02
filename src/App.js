0897b4349dc351c40966412bfdea062ad66594ad
skanthan39 <skanthan39@gmail.com>
2024-08-02 16:34:58 +0530

    my portfolio

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// 
+import TypingAnimation from './TypingAnimation.js';
+
+function App() {
+  return (
+    <div className="App">
+      <TypingAnimation/>
+    </div>
+  );
+}
+
+export default App;
